7de288aa9c5b18ea5bdee0f784da47cd1e6baa53
Chris Jennings <jenningschr@saic.com>
2014-02-16 09:16:49 -0800

    Updated for Om 0.4.1

--- src/leiningen/new/om_start/project.clj
+++ src/leiningen/new/om_start/project.clj
@@ -10,9 +10,9 @@
   :source-paths ["src/clj" "src/cljs"]
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.3.4"]
+                 [om "0.4.1"]
                  [com.facebook/react "0.8.0.1"]]
   
   :plugins [[lein-cljsbuild "1.0.2"]]
